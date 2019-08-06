493ed43522b2c258d2dd943a67bad9b7551d5dac
HYyoung <1045525786@qq.com>
2019-08-06 17:51:20 +0800

    主程修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("再次向服务器中提交");
+		System.out.println("主程再次向服务器中提交");
     }
 }
